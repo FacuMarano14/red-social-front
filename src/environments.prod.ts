45cbc8cfd6a4bad7455b2f8edcd3b1d303515013
FacuMarano14 <facundomarano06@gmail.com>
2025-11-05 08:26:40 -0300

    feat(deploy): Conecta environment.prod.ts a la API de Render

---
+++ src/environments.prod.ts
@@ -0,0 +1,9 @@
+export const environment = {
+  production: true,
+
+  apiUrl: 'https://red-social-api-070a.onrender.com/api', 
+
+  storageKey: 'auth_token_rs', 
+
+  appName: 'Red Social - PROD',
+};
